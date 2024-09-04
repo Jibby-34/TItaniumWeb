0ff21106e29da76f5ef2d78fef7410027d4541e5
Gibson DeGroot <20120725@zpsstudents.org>
2024-09-04 08:58:26 -0400

    Add script reference

--- uploader.js
+++ uploader.js
@@ -1,14 +1,14 @@
 const { ticalc, tifiles } = require('ticalc-usb');
 
+document.getElementById('chooseButton').addEventListener('click', await chooseCalc);
+document.getElementById('models').innerHTML = models();
+
 async function chooseCalc() {
     document.getElementById('buttonPressed').innerHTML = "Button Pressed";
     await ticalc.choose();
 }
 
 await ticalc.init();
-
-document.getElementById('chooseButton').addEventListener('click', await chooseCalc);
-document.getElementById('models').innerHTML = models();
 
 
 
