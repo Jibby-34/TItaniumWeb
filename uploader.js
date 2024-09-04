3363e8f0268db3a15707ddf09027c203b1376af8
Gibson DeGroot <20120725@zpsstudents.org>
2024-09-04 09:01:23 -0400

    Debugging

--- uploader.js
+++ uploader.js
@@ -3,9 +3,8 @@ const { ticalc, tifiles } = require('ticalc-usb');
 document.getElementById('chooseButton').addEventListener('click', await chooseCalc);
 document.getElementById('models').innerHTML = models();
 
-async function chooseCalc() {
+function chooseCalc() {
     document.getElementById('buttonPressed').innerHTML = "Button Pressed";
-    await ticalc.choose();
 }
 
 await ticalc.init();
